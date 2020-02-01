3d935e486fbb0c4aed5bb87ad83b3c2d9b4e2c60
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-02-01 11:35:53 -0800

    API-endpoints update

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 export default {
-  FOLDERS_API_ENDPOINT: `https://polar-tundra-09367.herokuapp.com/`,
-  NOTES_API_ENDPOINT: `https://polar-tundra-09367.herokuapp.com/`
+  FOLDERS_API_ENDPOINT: `https://polar-tundra-09367.herokuapp.com/api/folders`,
+  NOTES_API_ENDPOINT: `https://polar-tundra-09367.herokuapp.com/api/notes`
 };
